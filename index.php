eb02524a98c09bfd8811634fce920abdb68e0779
cioby09 <claudiociobanu09@gmail.com>
2023-05-19 11:08:12 +0200

    Exercise completed

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+require_once __DIR__ - "/Models/movie.php";
+
+$movies_array = [
+    $guardians = new Movie("Guardians of the Galaxy 3"),
+    $mario = new Movie("The Super Mario Bros. Movie"),
+]
+
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +20,14 @@
 </head>
 
 <body>
-
+    <h2>Film al cinema</h2>
+    <ul>
+        <?php foreach ($movies_array as $movie) { ?>
+            <li>
+                <?php echo $movie->printMovie() ?>
+            </li>
+        <?php } ?>
+    </ul>
 </body>
 
 </html>
